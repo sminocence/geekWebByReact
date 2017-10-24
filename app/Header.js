917a4c17b65d41ab34b66391e16e9556990268ea
sminocence <1090124891@qq.com>
2017-10-24 22:35:18 +0800

    用scrollIntoView方法代替锚链接

--- app/Header.js
+++ app/Header.js
@@ -2,15 +2,31 @@ import React,{Component} from 'react';
 import logoImg from './img/logo.png';
 
 class Header extends Component{
-     render(){
+
+    constructor(){
+        super();
+        this.handleAnchorClick = this.handleAnchorClick.bind(this);
+    }
+
+    handleAnchorClick(anchorName){
+        if(anchorName){
+            let anchorElement = document.getElementById(anchorName);
+            if(anchorElement){
+                anchorElement.scrollIntoView();
+                console.log(anchorName);
+            }
+        }
+    }
+
+    render(){
             return(
                 <div id="Logonav" className="Logonav">
                     <a href="#Home_describe"><img src={logoImg} /></a>
                     <ul className="nav">
-                        <li><a href="#Home_describe">工作室简介</a></li>
-                        <li><a href="#member_describe">成员介绍</a></li>
-                        <li><a href="#graduate_go">毕业去向</a></li>
-                        <li><a href="#join_us">加入我们</a></li>
+                        <li><a onClick={() => this.handleAnchorClick('Home_describe')}>工作室简介</a></li>
+                        <li><a onClick={ () => this.handleAnchorClick('member_describe')}>成员介绍</a></li>
+                        <li><a onClick={ () => this.handleAnchorClick('graduate_go')}>毕业去向</a></li>
+                        <li><a onClick={() => this.handleAnchorClick('join_us')}>加入我们</a></li>
                     </ul>
                 </div>
             );
